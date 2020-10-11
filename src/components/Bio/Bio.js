007ffd138a340c41bd06f3e225261197f55b4a9e
Ricardo J González C <rjgcastillo@hotmail.com>
2020-10-11 15:53:43 -0500

    Update bio component

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -6,50 +6,13 @@
  */
 
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
 
 import { rhythm } from 'utils/typography';
 import './Bio.css';
 
 import SocialBar from '../SocialBar';
-
-function Bio() {
-  return (
-    <StaticQuery
-      // eslint-disable-next-line no-use-before-define
-      query={bioQuery}
-      render={data => {
-        const { author, description } = data.site.siteMetadata;
-        return (
-          <div
-            style={{
-              marginBottom: rhythm(2.5),
-            }}
-          >
-            <div className="bio">
-              <Image
-                fixed={data.avatar.childImageSharp.fixed}
-                alt={author}
-                className="pic"
-                style={{
-                  marginRight: rhythm(1 / 2),
-                }}
-                imgStyle={{
-                  borderRadius: '50%',
-                }}
-              />
-              <div className="description">
-                <p>{description}</p>
-                <SocialBar />
-              </div>
-            </div>
-          </div>
-        );
-      }}
-    />
-  );
-}
 
 const bioQuery = graphql`
   query BioQuery {
@@ -68,5 +31,27 @@ const bioQuery = graphql`
     }
   }
 `;
+
+function Bio() {
+  const { avatar, site } = useStaticQuery(bioQuery);
+  const { author, description } = site.siteMetadata;
+  return (
+    <div style={{marginBottom: rhythm(2.5)}}>
+      <div className="bio">
+        <Image
+          alt={author}
+          className="pic"
+          fixed={avatar.childImageSharp.fixed}
+          imgStyle={{borderRadius: '50%'}}
+          style={{marginRight: rhythm(1 / 2)}}
+        />
+        <div className="description">
+          <p>{description}</p>
+          <SocialBar />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Bio;
