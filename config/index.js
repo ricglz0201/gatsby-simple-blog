7468335f6e000606633b6b7cb01821d78484ac1d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-12-28 22:01:15 +0000

    Deploy ricglz0201/gatsby-simple-blog to github.com/ricglz0201/gatsby-simple-blog.git:gh-pages

---
+++ config/index.js
@@ -0,0 +1,26 @@
+const pathPrefix = '/gatsby-simple-blog';
+const githubUsername = 'ricglz0201';
+const site = {
+  pathPrefix,
+  title: 'Gatsby Starter Blog',
+  author: 'Ricardo González',
+  description: 'A starter blog demonstrating what Gatsby can do.',
+  siteUrl: `https://${githubUsername}.github.io${pathPrefix}`,
+  twitter: 'ricglz',
+  github: githubUsername,
+  disqusShortName: 'gatsby-simple-blog',
+  lang: 'en',
+  displayTranslations: true,
+  postsPerPage: 3,
+};
+
+const supportedLanguages = {
+  en: 'English',
+  es: 'Español',
+  fr: 'Français',
+};
+
+module.exports = {
+  site,
+  supportedLanguages,
+};
